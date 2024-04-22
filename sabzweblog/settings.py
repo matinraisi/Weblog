9eed38d16fdee4b41d510b34b702fafbbcd0e0cc
matinraisi <raisi.m630@gmail.com>
2024-04-22 11:46:48 +0330

    install postgresql & connect database and backup data insrt new database

--- sabzweblog/settings.py
+++ sabzweblog/settings.py
@@ -76,10 +76,19 @@ WSGI_APPLICATION = 'sabzweblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "blog",
+        'USER':'blog',
+        'PASSWORD':'blog1234',
+        'PORT':'5432',
     }
 }
 
